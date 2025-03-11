0ac0b1ba81286e0c60bab2cc982c0fb6fb748002
岩堀真吾 <iwahori@std-one.com>
2025-03-11 12:34:44 +0900

    jsのjQuery依存を排除

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-const $ = require('jquery');
+// const $ = require('jquery');
 
 // import Swiper from 'swiper/bundle';
 // import 'swiper/css/bundle';
@@ -11,6 +11,7 @@ const hogehoge = (() => {
 
 	// 変数宣言
 	const pointSP = window.matchMedia('(max-width: 767px)');
+	const body = document.querySelector('body');
 
 	// このJSのinit
 	function init() {
@@ -33,12 +34,81 @@ const hogehoge = (() => {
 	}
 
 	// 関数 ------------------------------------
-	function fixedBG() {
-		$('body').addClass('is-fixed');
+	function toggleFixedBG(add) {
+		add ? body.classList.add('is-fixed') : body.classList.remove('is-fixed');
 	}
 
-	function fixedBGReset() {
-		$('body').removeClass('is-fixed');
+	// slideUp
+	function slideUp(el, duration = 400) {
+		el.style.height = el.offsetHeight + "px";
+		el.offsetHeight; // Force reflow
+		el.style.transitionProperty = "height, margin, padding";
+		el.style.transitionDuration = duration + "ms";
+		el.style.transitionTimingFunction = "ease";
+		el.style.overflow = "hidden";
+		el.style.height = 0;
+		el.style.paddingTop = 0;
+		el.style.paddingBottom = 0;
+		el.style.marginTop = 0;
+		el.style.marginBottom = 0;
+
+		setTimeout(function() {
+			el.style.display = "none";
+			el.style.removeProperty("height");
+			el.style.removeProperty("padding-top");
+			el.style.removeProperty("padding-bottom");
+			el.style.removeProperty("margin-top");
+			el.style.removeProperty("margin-bottom");
+			el.style.removeProperty("overflow");
+			el.style.removeProperty("transition-duration");
+			el.style.removeProperty("transition-property");
+			el.style.removeProperty("transition-timing-function");
+		}, duration);
+	}
+
+	// slideDown
+	function slideDown(el, duration = 400) {
+		el.style.removeProperty("display");
+		let display = window.getComputedStyle(el).display;
+		if (display === "none") {
+			display = "block";
+		}
+		el.style.display = display;
+
+		let height = el.offsetHeight;
+		el.style.overflow = "hidden";
+		el.style.height = 0;
+		el.style.paddingTop = 0;
+		el.style.paddingBottom = 0;
+		el.style.marginTop = 0;
+		el.style.marginBottom = 0;
+		el.offsetHeight; // Force reflow
+
+		el.style.transitionProperty = "height, margin, padding";
+		el.style.transitionDuration = duration + "ms";
+		el.style.transitionTimingFunction = "ease";
+		el.style.height = height + "px";
+		el.style.removeProperty("padding-top");
+		el.style.removeProperty("padding-bottom");
+		el.style.removeProperty("margin-top");
+		el.style.removeProperty("margin-bottom");
+
+		setTimeout(function() {
+			el.style.removeProperty("height");
+			el.style.removeProperty("overflow");
+			el.style.removeProperty("transition-duration");
+			el.style.removeProperty("transition-property");
+			el.style.removeProperty("transition-timing-function");
+		}, duration);
+	}
+
+	// slideToggle
+	function slideToggle(el, duration = 400) {
+		if (window.getComputedStyle(el).display === "none") {
+			slideDown(el, duration);
+		} else {
+			slideUp(el, duration);
+		}
 	}
 
 	function scrollAnimation() {
@@ -66,44 +136,74 @@ const hogehoge = (() => {
 	}
 
 	function smoothScroll() {
-		$('a[href^="#"]').on('click.smoothScroll', (event) => {
-			const speed = 1200;// ミリ秒
-			const href = $(event.currentTarget).attr("href");
-			const target = $(href == "#" || href == "" ? 'html' : href);
-			const position = target.offset().top;
-			$('html, body').animate({ scrollTop: position }, speed, 'swing');
-			return false;
+		const links = document.querySelectorAll('a[href^="#"]');
+		links.forEach(link => {
+			link.addEventListener('click', function(event) {
+				event.preventDefault();
+				const href = this.getAttribute('href');
+				const target = document.querySelector(href === "#" || href === "" ? 'html' : href);
+				if (target) {
+					const targetPosition = target.offsetTop;
+					window.scrollTo({
+						top: targetPosition,
+						behavior: 'smooth'
+					});
+				}
+			});
 		});
 	}
 
 	function menuTrigger() {
-		const $trigger = $('[aria-controls="l-nav-global"]');
-		const $nav = $('.' + $trigger.attr('aria-controls'));
+		const triggers = document.querySelectorAll('[aria-controls="l-nav-global"]');
+		const nav = document.querySelector('.l-nav-global');
+		const anchors = nav.querySelectorAll('a[href*="#"]');
 
-		$trigger.on('click', () => {
-			const expanded = $trigger.attr('aria-expanded');
+		function handleTriggerClick(trigger) {
+			const expanded = trigger.getAttribute('aria-expanded') === 'true';
 
-			if (expanded === 'false') {
-				$trigger.attr('aria-expanded', true);
-				$nav.attr('aria-hidden', false);
-				fixedBG();
-			} else {
-				$trigger.attr('aria-expanded', false);
-				$nav.attr('aria-hidden', true);
-				fixedBGReset();
-			}
+			triggers.forEach(trigger => {
+				trigger.setAttribute('aria-expanded', !expanded);
+			});
+
+			nav.setAttribute('aria-hidden', expanded);
+			toggleFixedBG(!expanded);
+		}
+
+		triggers.forEach(trigger => {
+			trigger.addEventListener('click', () => handleTriggerClick(trigger));
 		});
 
 		function checkBreakPoint(mq) {
 			if (!mq.matches) {
-				$trigger.attr('aria-expanded', false);
-				$nav.attr('aria-hidden', true);
-				fixedBGReset();
+				triggers.forEach(trigger => {
+					trigger.setAttribute('aria-expanded', 'false');
+				});
+				nav.setAttribute('aria-hidden', 'true');
+				toggleFixedBG(false);
 			}
 		}
 
 		pointSP.addEventListener('change', checkBreakPoint);
-		// checkBreakPoint(pointSP);
+
+		// anchors.forEach(anchor => {
+		// 	anchor.addEventListener('click', (event) => {
+		// 		triggers.forEach(trigger => {
+		// 			trigger.setAttribute('aria-expanded', 'false');
+		// 		});
+		// 		nav.setAttribute('aria-hidden', 'true');
+		// 		toggleFixedBG(false);
+
+		// 		const href = anchor.getAttribute('href');
+		// 		const targetId = href.split('#')[1];
+		// 		const targetElement = document.getElementById(targetId) || document.documentElement;
+		// 		const position = targetElement.offsetTop;
+		// 		window.scrollTo({
+		// 			top: position,
+		// 			behavior: 'smooth'
+		// 		});
+		// 		event.preventDefault();
+		// 	});
+		// });
 	}
 
 	function telLink() {
@@ -143,53 +243,65 @@ const hogehoge = (() => {
 	}
 
 	function carousel() {
-		const $carousel = $('.js-carousel');
-		if($carousel.length) {
-			$carousel.slick({
-				autoplay: true,
-				autoplaySpeed: 0,
-				arrows: false,
-				cssEase: 'linear',
-				speed: 10000,
-				variableWidth: true
+		const carousel = document.querySelector('.js-carousel');
+		if (carousel) {
+			const Swiper = new Swiper(carousel, {
+				autoplay: {
+					delay: 5000,
+				},
+				centeredSlides: true,
+				lazy: true,
+				loop: true,
+				slidesPerView: 'auto',
+				spaceBetween: 30,
 			});
 		}
-
-		const swiper = new Swiper('.js-carousel', {
-			autoplay: {
-				delay: 5000,
-			},
-			centeredSlides: true,
-			lazy: true,
-			loop: true,
-			slidesPerView: 'auto',
-			spaceBetween: 30,
-		});
 	}
 
 	function modal() {
-		const $trigger = $('[aria-controls*="modal"]');
-		$trigger.off('click.smoothScroll').on('click', (event) => {
-			const $self = $(event.currentTarget);
-			const expanded = $self.attr('aria-expanded');
-			const $target = $('#' + $self.attr('aria-controls'));
-			const $other = $target.find('[aria-controls*="modal"]');
+		const triggers = document.querySelectorAll('[aria-controls*="modal"]');
 
-			if (expanded === 'false') {
-				$self.attr('aria-expanded', true).addClass('is-active');
-				$other.attr('aria-expanded', true);
-				$target.attr('aria-hidden', false).addClass('is-active');
+		triggers.forEach(trigger => {
+			trigger.addEventListener('click', (event) => {
+				event.preventDefault();
+				const targetId = trigger.getAttribute('aria-controls');
+				const target = document.getElementById(targetId);
+				const expanded = trigger.getAttribute('aria-expanded') === 'true';
 
-				fixedBG();
-			} else {
-				$trigger.attr('aria-expanded', false).removeClass('is-active');
-				$target.attr('aria-hidden', true).removeClass('is-active');
+				// 他のモーダルをすべて閉じる
+				document.querySelectorAll('[id*="modal"]').forEach(modal => {
+					if (modal.id !== targetId) {
+						closeModal(modal);
+					}
+				});
 
-				fixedBGReset();
-			}
+				if (expanded) {
+					closeModal(target);
+				} else {
+					openModal(target);
+				}
 
-			return false;
+				toggleFixedBG(!expanded);
+			});
 		});
+
+		function closeModal(modal) {
+			modal.setAttribute('aria-hidden', 'true');
+			modal.classList.remove('is-active');
+			document.querySelectorAll(`[aria-controls="${modal.id}"]`).forEach(trigger => {
+				trigger.setAttribute('aria-expanded', 'false');
+				trigger.classList.remove('is-active');
+			});
+		}
+
+		function openModal(modal) {
+			modal.setAttribute('aria-hidden', 'false');
+			modal.classList.add('is-active');
+			document.querySelectorAll(`[aria-controls="${modal.id}"]`).forEach(trigger => {
+				trigger.setAttribute('aria-expanded', 'true');
+				trigger.classList.add('is-active');
+			});
+		}
 	}
 
 	function tab() {
@@ -211,19 +323,24 @@ const hogehoge = (() => {
 	}
 
 	function accordion() {
-		const $trigger = $('[aria-controls*="accordion"]');
-		$trigger.stop().on('click', (event) => {
-			const $self = $(event.currentTarget);
-			const expanded = $self.attr('aria-expanded');
-			const $target = $('#' + $self.attr('aria-controls'));
+		const triggers = document.querySelectorAll('[aria-controls*="accordion"]');
 
-			if (expanded === 'false') {
-				$self.attr('aria-expanded', true);
-				$target.attr('aria-hidden', false).slideDown();
-			} else {
-				$self.attr('aria-expanded', false);
-				$target.attr('aria-hidden', true).slideUp();
-			}
+		triggers.forEach(trigger => {
+			trigger.addEventListener('click', (event) => {
+				const self = event.currentTarget;
+				const expanded = self.getAttribute('aria-expanded') === 'true';
+				const target = document.getElementById(self.getAttribute('aria-controls'));
+
+				if (!expanded) {
+					self.setAttribute('aria-expanded', 'true');
+					target.setAttribute('aria-hidden', 'false');
+					slideDown(target);
+				} else {
+					self.setAttribute('aria-expanded', 'false');
+					target.setAttribute('aria-hidden', 'true');
+					slideUp(target);
+				}
+			});
 		});
 	}
 	// -----------------------------------------
